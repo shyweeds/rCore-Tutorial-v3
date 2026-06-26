e20d1ca28d3e4bad976cd2d831799aba7fd48217
shyweeds <2149934895@qq.com>
2026-06-26 15:30:23 +0800

    optimize the code

--- os/src/mm/frame_allocator.rs
+++ os/src/mm/frame_allocator.rs
@@ -76,7 +76,7 @@ impl FrameAllocator for StackFrameAllocator {
     fn dealloc(&mut self, ppn: PhysPageNum) {
         let ppn = ppn.0;
         // validity check
-        if ppn >= self.current || self.recycled.iter().any(|&v| v == ppn) {
+        if ppn >= self.current || self.recycled.contains(&ppn) {
             panic!("Frame ppn={:#x} has not been allocated!", ppn);
         }
         // recycle
